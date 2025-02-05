330e32dd8c7121d138442d06f54ab13a8b4e109a
Ihar Abukhouski <ihar.abukhouski@gmail.com>
2025-02-04 21:31:44 -0600

    wip

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .openai import o3_mini
+from .claude import sonnet, haiku
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup
+from setuptools import setup #, find_packages
 
 setup(
   name='llm',
   version='0.0.1',
+  packages=['llm'],
+  # package_dir={"": "src"},
+  # packages=find_packages(where="src"),
   install_requires=[
     'openai==1.61.0',
     'anthropic==0.45.2',
